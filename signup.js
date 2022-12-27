615eb89d64d804d54659429e41fcb26d0159692c
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2022-12-27 10:41:38 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -13,7 +13,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebas
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
+  
    const firebaseConfig = {
     apiKey: "AIzaSyBo1Gd377Glpbjsi8iAXWUENl4gqA203m4",
     authDomain: "marketing-web-c37fb.firebaseapp.com",
@@ -23,7 +23,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebas
     messagingSenderId: "893694529509",
     appId: "1:893694529509:web:845deca21c21a0215e290c"
   };
-  };
+
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
